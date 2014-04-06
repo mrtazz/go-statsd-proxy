1cd4017ae69b6a5e6731fa60f4711b8e7fea3de0
Daniel Schauenberg <d@unwiredcouch.com>
2014-04-06 14:00:41 -0400

    add example config file

---
+++ exampleProxyConfig.js
@@ -0,0 +1,10 @@
+{
+  "nodes": [
+    {"host": "127.0.0.1", "port": 8130, "adminport": 8140},
+    {"host": "127.0.0.1", "port": 8131, "adminport": 8141},
+    {"host": "127.0.0.1", "port": 8132, "adminport": 8142}
+  ],
+  "host":  "0.0.0.0",
+  "port": 8125,
+  "checkInterval": 1000
+}
