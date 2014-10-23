9ac585ddf2ad064f43e2ad8f63de1c8be12a8a1d
AJ Christensen <aj@junglistheavy.industries>
2014-10-23 15:32:59 -0700

    Add sigterm/interrupt handling

--- main.go
+++ main.go
@@ -3,16 +3,42 @@ package main
 
 import (
 	"flag"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/mrtazz/go-statsd-proxy/statsdproxy"
 )
 
+var (
+	configfile = flag.String("f", "/etc/statsdproxy.json", "Configuration file path")
+	debug      = flag.Bool("d", false, "enable debug mode")
+)
+
 func main() {
-	// note, that variables are pointers
-	configfile := flag.String("f", "", "config file path")
-	debug := flag.Bool("d", false, "enable debug mode")
 	flag.Parse()
+
 	statsdproxy.DebugMode = *debug
-  // TODO: catch sigterm and actually tell the process to quit
-  quit := make(chan bool)
-	statsdproxy.StartProxy(*configfile, quit)
+
+	quit := make(chan bool)
+
+	handleSignals(quit)
+
+	statsdproxy.StartProxy(
+		*configfile,
+		quit,
+	)
+}
+
+func handleSignals(quitChan chan bool) {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM, syscall.SIGHUP)
+	go func(chan os.Signal) {
+		for sig := range c {
+			if sig == os.Interrupt || sig == syscall.SIGTERM {
+				quitChan <- true
+				os.Exit(0)
+			}
+		}
+	}(c)
 }
